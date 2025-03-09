ebe04c807fb42a5f99e799c4741038e343cb301c
narender347 <narendarhoney@gmail.com>
2025-03-10 00:34:12 +0530

    Update sweep_code.py

--- sweep_code.py
+++ sweep_code.py
@@ -25,11 +25,11 @@ sweep_id = wandb.sweep(sweep_config, project="fashion-mnist-hyperparameter-tunin
 # Load dataset from Fashion MNIST
 (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
 
-# Normalize data
+# Normalizing the  data
 X_train = X_train.reshape(X_train.shape[0], -1) / 255.0  
 X_test = X_test.reshape(X_test.shape[0], -1) / 255.0
 
-# Manually split validation set (10% of training data)
+# splitting for the validation set (10% of training data)
 val_size = int(0.1 * X_train.shape[0])
 indices = np.random.permutation(X_train.shape[0])  
 
